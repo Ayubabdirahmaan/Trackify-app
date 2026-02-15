6302395ac195b26a3508933d52e629057608b302
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-15 19:56:53 +0300

    connection Db

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 const app = express();
-app.use(express.json());
 import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 dotenv.config();
 const PORT = process.env.PORT;
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.json("this is finanace app, good afternoon");
 });
+mongoose
+  .connect(process.env.MONGO_URI_PRO)
+  .then(() => console.log("✅ Connection Successfully"))
+  .catch((error) => console.log("❌error connection", error));
 app.listen(PORT, () => {
   console.log(`server is runing on http://localhost:${PORT}`);
 });
