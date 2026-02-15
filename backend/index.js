350fcf2c075f80b41673a0b8527fa1ad082a1a7f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-15 17:36:22 +0300

    added new packeges

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 import express from "express";
 const app = express();
 app.use(express.json());
-import dotenv from 'dotenv'
-dotenv.config()
-const PORT = process.env.PORT
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.json("this is finanace app");
+  res.json("this is finanace app, good afternoon");
 });
-
-
 app.listen(PORT, () => {
   console.log(`server is runing on http://localhost:${PORT}`);
 });
