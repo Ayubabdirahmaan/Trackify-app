0cfac5ea91871b5bd77fda5b5529caa32acfe965
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-15 17:47:11 +0300

    added querycleint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query/build/legacy";
+import { QueryClient } from "node_modules/@tanstack/react-query/build/legacy";
 
-createRoot(document.getElementById('root')).render(
+const QueryClient = new QueryClient({
+  defaultOption: {
+    query: {
+      stalTime: 60 * 1000,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={QueryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
