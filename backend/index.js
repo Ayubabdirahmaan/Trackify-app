4aac5b53622355b7e6369542c3c242e7e53e3728
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-15 20:43:34 +0300

    added middlewares and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import { notFound } from "./middlewares/notFound.js";
+import { errorHandle } from "./middlewares/errorHandle.js";
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
@@ -13,6 +15,9 @@ app.use(cors({
 app.get("/", (req, res) => {
   res.json("this is finanace app, good afternoon");
 });
+
+app.use(notFound)
+app.use(errorHandle)
 mongoose
   .connect(process.env.MONGO_URI_PRO)
   .then(() => console.log("✅ Connection Successfully"))
---
+++ backend/schema/userAuthSchema.js
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const userSchema = z.object({
+    name: z.string().min(1, 'name is required'),
+    email: z.string().email('email must be valid'),
+    password: z.string()
+    .min(8, 'password must be at least 8 characters')
+    .max(100, 'password must be at most 100 character')
+})
