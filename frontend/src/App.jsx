a1e84f3973e26ef77e20a490a150de285fda2d55
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-17 14:44:06 +0300

    feat: landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,12 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
-import { Testpage } from "./Tastpage";
+import { LadingPage } from "./Pages/ladingPage";
+
+
 function App() {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["User"],
-    queryFn: () => 
-      fetch("http://localhost:2000/api/user/AllUser").then((res) =>
-        res.json(),
-      ),
-  });
-  if (isLoading) return <h2>isloading...</h2>;
-  if (error) return <h2>error..</h2>;
   return (
-   <>
-     <Testpage />
     <div>
-     
-      { data.user?.map((users) => (
-        <h2>{users.name}</h2>
-      ))}
-    </div></>
-  );
+     <LadingPage />
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/Pages/LadingPage.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import logoTracker from "../assets/logoTracker.svg";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+export const LadingPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      {/* header section */}
+      <header className=" flex justify-between items-center m-3">
+        {/* logo */}
+        <div className="flex items-center">
+          <div>
+            <img src={logoTracker} className="w-20 h-20" alt="" />
+          </div>
+          <div className="text-xl font-semibold text-primary cursor-pointer ">
+            Trackify-app
+          </div>
+        </div>
+        {/* singIn */}
+        <div>
+          <Link
+            to="/signin"
+            className="bg-primary text-secondary p-3 w-10 h-10 rounded mt-2"
+          >
+            SignIn
+          </Link>
+        </div>
+      </header>
+      {/* hero section */}
+      <div className="flex flex-col justify-center text-center mt-15">
+        <div className="mb-5">
+          {/* hero title */}
+          <h2 className="text-3xl font-semibold text-primary">
+            Smart Financial Tracking for Better Money Decisions Every Day
+          </h2>
+          {/* description */}
+        </div>
+        <div>
+          {" "}
+          <p className="text-lg">
+            Track expenses, analyze trends, and stay in control of your
+            financial life.
+          </p>
+        </div>
+        <div className="flex justify-center gap-3 mt-10">
+          <div className="">
+            <Button className="rounded-full">Get Started Free</Button>
+          </div>
+          <div>
+            <Button className="rounded-full bg-destructive">Learn More</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
